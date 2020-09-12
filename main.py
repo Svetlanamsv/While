68987dcbb1a627dfe5157e28d8c02acf2d33fd98
Svetlanamsv <svetikmsv34@gmail.com>
2020-09-12 17:47:25 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+def myfunction(x,y,z):
+    if  z == 0:
+        print ("The third number is not correct number")
+    else:
+       while x <= y:
+        x = x + z
+       else:
+         print("The first number plus the third number  is greater than the second number")
+
+
+myfunction(1,1,1)
+myfunction(1,1,0)
+myfunction(1,6,1)
+
+
+
